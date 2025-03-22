3688786e288766931a72e122e69a500731911118
Derek Paul <dinosaurderek@Dereks-MacBook-Pro.local>
2025-03-21 19:42:42 -0700

    adds basic wallet connection and network switching with native currency display

---
+++ src/lib/wagmiConfig.js
@@ -0,0 +1,16 @@
+import { createConfig } from 'wagmi'
+import { base, bsc, mainnet, polygon, sepolia } from 'viem/chains'
+import { http } from 'viem'
+
+const wagmiConfig = createConfig({
+  chains: [mainnet, base, bsc, polygon, sepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+    [bsc.id]: http(),
+    [polygon.id]: http(),
+    [sepolia.id]: http(),
+  },
+})
+
+export { wagmiConfig };
